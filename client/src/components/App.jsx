99bac26c2cd2263abe1611fbb39f9eb1f8a83d70
Fernando <ferdelamad@gmail.com>
2018-11-02 18:59:51 -0700

    Added a the Header component and styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: true,
+    };
+  }
+
   render() {
-    return <h1>I'am React</h1>;
+    return (
+      <div className="container">
+        <Header />
+      </div>
+    );
   }
 }
